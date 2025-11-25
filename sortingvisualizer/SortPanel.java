4e235d46c8b4d8a089e8c833212c08460097ae26
Vishal Choudhary <vishalchoudhary7757@gmail.com>
2025-11-25 10:42:09 +0530

    feat: introduce SortPanel component for interactive sorting visualization with highlighting capabilities.

--- sortingvisualizer/SortPanel.java
+++ sortingvisualizer/SortPanel.java
@@ -3,7 +3,6 @@ package sortingvisualizer;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
-import sortingvisualizer.SortStep.StepType;
 
 public class SortPanel extends JPanel {
     private static final long serialVersionUID = 1L;
